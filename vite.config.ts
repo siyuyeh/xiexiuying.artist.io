36b4577e6db160150fd130ba7d908c42f33efcfc
siyuyeh <mia791003+tw@gmail.com>
2025-04-20 10:02:21 +0800

    準備 deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/xiexiuying.artist.io/',    // ← 加上這行
   plugins: [react()],
   server: {
-    port: 1420
+    port: 1420,
+    strictPort: true
   }
 });
